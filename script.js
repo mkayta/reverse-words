9f745fa3af1c857478eb2f1f202b92d709f51ca8
Eng-Mohamed <mkayta2023@gmail.com>
2024-10-01 19:32:28 +0300

    I done this Challenge

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Good Luck 💪🏾
+function reverseWords(sentence){
+    let sentencesplit=sentence.split(" ");
+    let sentencereverse=sentencesplit.reverse();
+    let sentencejoin=sentencereverse.join(" ");
+    return sentencejoin
+
+}
+
+console.log(reverseWords("fikrcamp bootcamp")) 
+console.log(reverseWords("hello there!")) 
+console.log(reverseWords("welcome to my city")) 
